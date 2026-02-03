f8f0f44da3c0e56f96ecc91da6d4006b1ae8a082
Nelson Castanha <dorto@live.com>
2026-02-03 00:23:49 -0300

    Story Button

---
+++ packages/docs/eslint.config.js
@@ -0,0 +1,33 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+export default defineConfig([globalIgnores(['dist']),
+
+//legacy shared config
+...compat.extends('@ignite-ui/eslint-config'),
+
+{
+  files: ['**/*.{ts,tsx}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+}, ...storybook.configs["flat/recommended"]])
