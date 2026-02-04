ebdf77fc415e87b28c806e1fd565d8adee5f0962
Nelson Castanha <dorto@live.com>
2026-02-04 13:42:15 -0300

    added tokens page on Storybook with colors

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,24 +1,19 @@
+import { dirname } from 'path'
 
-
-import { dirname } from "path"
-
-import { fileURLToPath } from "url"
+import { fileURLToPath } from 'url'
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value) {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
-  ],
-  "addons": [],
-  "framework": getAbsolutePath('@storybook/react-vite')
-};
-export default config;
+  stories: ['../src/pages/**/*.mdx', '../src/stories/**/*.stories.tsx'],
+  addons: [getAbsolutePath('@storybook/addon-docs')],
+  framework: getAbsolutePath('@storybook/react-vite'),
+}
+export default config
--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,13 +1,19 @@
 /** @type { import('@storybook/react-vite').Preview } */
+
+import { themes } from 'storybook/theming'
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    docs: {
+      theme: themes.dark,
+    },
   },
-};
+}
 
-export default preview;
+export default preview
